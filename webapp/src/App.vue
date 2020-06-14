cbecc84b033dc0cb64a44173254db303ae837afe
Vivien <27066260+vivienlouradour@users.noreply.github.com>
2020-06-14 17:02:11 +0200

    webapp: fix header position on small device

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,12 +1,12 @@
 
 <template>
   <div id="app">
-
-       <div class="row justify-content-md-center">
-         
-      <h2>Partagez vos sons : liste des publications</h2>
-       </div>
-    <PostTable></PostTable>
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <h2>Partagez vos sons : liste des publications</h2>
+      </div>
+      <PostTable></PostTable>
+    </div>
   </div>
 </template>
 
